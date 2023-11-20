16963b78ae29871413486ba31b441e452d68f6b4
Sayed Zakria <sayed_zakrea@hotmail.com>
2023-11-20 18:30:14 +0200

    Current weather api

--- WeatherApi/Controllers/WeatherForecastController.cs
+++ WeatherApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using WeatherApi.Models;
 
 namespace WeatherApi.Controllers
 {
@@ -10,12 +14,13 @@ namespace WeatherApi.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+        string apiKey = "708f580ebb294289bff144519232011";
         private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private readonly IHttpClientFactory _httpClientFactory;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
@@ -28,6 +33,36 @@ namespace WeatherApi.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        //Get Current city weather
+        [HttpGet(Name = "GetCurrent?city={city}&lang={lang}")]
+        public async Task<IActionResult> GetCurrentAsync(string city, string lang)
+        {
+            string apiUrl = $"https://api.weatherapi.com/v1/current.json?{city}=London&lang={lang}&key=" + apiKey;
+
+            using (HttpClient client = _httpClientFactory.CreateClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    CurrentWeather currentWeather = new CurrentWeather();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResult = await response.Content.ReadAsStringAsync();
+                        currentWeather = JsonConvert.DeserializeObject<CurrentWeather>(jsonResult) ?? new CurrentWeather();
+                        return StatusCode(200, currentWeather);
+                    }
+                    else
+                    {
+                        return StatusCode((int)response.StatusCode, $"Error: {response.ReasonPhrase}");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    return StatusCode(500, $"Error: {ex.Message}");
+                }
+            }
         }
     }
 }
---
+++ WeatherApi/Models/CurrentWeather.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApi.Models
+{
+    public class CurrentWeather
+    {
+        public Location location { get; set; }
+        public Current current { get; set; }
+    }
+}
